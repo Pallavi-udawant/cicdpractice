f06e85129f2dfa25b2a4dbf9ed573cfa2aabfe75
Pallavi Udawant <pallavi.udawant4590@gmail.com>
2024-11-23 14:47:04 +0630

    new gotestcode added

--- src/main_test.go
+++ src/main_test.go
@@ -8,7 +8,7 @@ func Test_printHi(t *testing.T) {
 	name := "Abhishek."
 	want := "Hi Abhishek."
 
-	if got := src.printHi(name); got != want {
+	if got := printHi(name); got != want {
 		t.Errorf("hello() = %q, want %q", got, want)
 	}
 }
